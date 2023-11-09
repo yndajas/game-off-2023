d5489108d32034b24f261acf9ce1e761245c5ba3
Ynda Jas <yndajas@gmail.com>
2023-11-09 00:42:49 +0000

    Add tutorial modal
    ... with placeholder text. We'll test this text properly once we've
    worked on the content
    
    Co-authored-by: Manan Shah <manan78101@gmail.com>

---
+++ Assets/Tests/Unit/UI/TutorialModalTest.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Tests.Unit.UI
+{
+    public class TutorialModalTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Menu.unity");
+        }
+
+        [Test]
+        public void ModalIsInactiveInitially()
+        {
+            var tutorialModalElement = GameObject.Find("TutorialModal");
+            Assert.IsNull(tutorialModalElement);
+        }
+        
+        [Test]
+        public void ModalTogglesVisibilityOnButtonClick()
+        {
+            var tutorialButton = new GameObject("TutorialButton");
+
+            var modalPanel = new GameObject("TutorialModal");
+            modalPanel.SetActive(false);
+            
+            var tutorialModalScript = tutorialButton.AddComponent<TutorialModal>();
+            tutorialModalScript.modalPanel = modalPanel;
+
+            tutorialModalScript.ToggleTutorialModal();
+            Assert.IsTrue(modalPanel.activeSelf);
+            
+            tutorialModalScript.ToggleTutorialModal();
+            Assert.IsFalse(modalPanel.activeSelf);
+        }
+    }
+}
