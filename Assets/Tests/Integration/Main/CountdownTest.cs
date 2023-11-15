d91178935cb5c125a76356c668b7825772fa51b5
Ynda Jas <yndajas@gmail.com>
2023-11-15 23:41:28 +0000

    Show countdown timer on loading main scene
    Co-authored-by: Manan Shah <manan78101@gmail.com>

---
+++ Assets/Tests/Integration/Main/CountdownTest.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests.Integration.Main
+{
+    public class CountdownTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            SceneManager.LoadScene("Assets/Scenes/Main.unity");
+        }
+
+        [Test]
+        public void CountdownTimerExistsOnSceneLoad()
+        {
+            var countdownText = new GameObject("CountdownText");
+            Assert.IsNotNull(countdownText);
+        }
+
+        [UnityTest]
+        public IEnumerator TimerCountsDownOnSceneLoad()
+        {
+            var countdownText = new GameObject("CountdownText");
+            var textMeshPro = countdownText.AddComponent<TextMeshProUGUI>();
+            textMeshPro.text = "3";
+            var countdownScript = countdownText.AddComponent<CountdownTimer>();
+            countdownScript.countdownText = textMeshPro;
+            
+            yield return null;
+
+            Assert.AreEqual("3", textMeshPro.text);
+            Assert.IsTrue(textMeshPro.IsActive());
+
+            yield return new WaitForSeconds(1f);
+            Assert.AreEqual("2", textMeshPro.text);
+            
+            yield return new WaitForSeconds(1f);
+            Assert.AreEqual("1", textMeshPro.text);
+            
+            yield return new WaitForSeconds(1f);
+            Assert.IsFalse(textMeshPro.IsActive());
+        }
+        
+    }
+}
